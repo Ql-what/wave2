a41929e2aee714aef5fde50a20a296122073529b
Ql-what <62632626+Ql-what@users.noreply.github.com>
2020-04-24 22:14:46 -0400

    Add files via upload

---
+++ colour of squares.py
@@ -0,0 +1,29 @@
+Horizontalletter = ['a', 'c', 'e', 'g']
+Horizontalletter2 = ['b', 'd', 'f', 'h']
+
+verticalnumber= ['1', '3', '5', '7']
+verticalnumber2 = ['2', '4', '6', '8']
+
+pos = input()
+let = pos[0]
+num = pos[1]
+
+if let in Horizontalletter:
+    if num in verticalnumber:
+        print('black')
+
+    else:
+        print('white')
+
+if let in Horizontalletter2:
+    if num in verticalnumber:
+        print('white')
+
+    else:
+        print('black')
+
+
+
+
+
+
